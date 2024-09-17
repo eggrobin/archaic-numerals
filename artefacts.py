cd7b1e99598fd020dc9f610477fd800902958a1c
Robin Leroy <egg.robin.leroy@gmail.com>
2024-09-18 00:51:42 +0200

    missed links

--- artefacts.py
+++ artefacts.py
@@ -1,10 +1,7 @@
 import html.parser
 import re
-from typing import Optional
+from typing import Literal, Optional
 import urllib.request
-from TexSoup import TexSoup
-from TexSoup.data import TexNode, BraceGroup, BracketGroup
-
 
 def deromanize(s: str) -> int:
   numerals = {'i': 1, 'v': 5, 'x': 10, 'l': 50, 'c': 100, 'd': 500, 'm': 1000}
@@ -50,9 +47,12 @@ class Parser(html.parser.HTMLParser):
 
     def handle_data(self, data : str):
         if self.xlabel_start:
-           self.line_to_id[tuple(str(deromanize(s)) if re.match(r'^[ivxlcdm]+$', s) else s for s in data.split())] = self.line_id
+           line_number = tuple(str(deromanize(s)) if re.match(r'^[ivxlcdm]+$', s) else s for s in data.split())
+           if artefact == 'P010586' and line_number[0] not in ('o', 'r'):
+             line_number = ('o', *line_number)
+           self.line_to_id[line_number] = self.line_id or None
 
-def get_line_to_id_map(project: str, artefact):
+def get_line_to_id_map(project: str, artefact: str):
   with urllib.request.urlopen('https://oracc.museum.upenn.edu/' + project + '/' + artefact) as f:
     page = f.read().decode('utf-8')
   parser = Parser()
@@ -62,38 +62,71 @@ def get_line_to_id_map(project: str, artefact):
 with open('archaic-numerals.tex', encoding='utf-8') as f:
    source = f.read()
 
-citations : list[TexNode] = []
+class Argument:
+  position: int
+  string: str
+  brackets:  Literal['[]', '{}']
 
-soup = TexSoup(source, tolerance=1)
-for command in ('cite', 'cites'):
-  for node in soup.find_all(command):
-    if any(isinstance(arg, BraceGroup) and re.match(r'^[PQ][0-9]+$', arg.string) for arg in node.args):
-      citations.append(node)
+  def __init__(self, position : int, string : str, brackets : Literal['[]', '{}']):
+    self.position = position
+    self.string = string
+    self.brackets = brackets
+
+  def __repr__(self):
+    return f"Argument({self.position}, {repr(self.string)})"
+
+citations : list[list[Argument]] = []
+
+for match in re.finditer(r'\\cites?(?=[\[{])', source):
+  i = match.end()
+  depth = 0
+  start = i
+  args : list[Argument] = []
+  while i < len(source):
+    c = source[i]
+    if c in ('[', '{'):
+      depth += 1
+      i += 1
+    elif c in (']', '}'):
+      depth -= 1
+      if depth == 0:
+        args.append(Argument(start, source[start+1:i], '[]' if c == ']' else '{}'))
+        start = i+1
+      i += 1
+    elif depth == 0:
+      break
+    elif c == '\\':
+      i += 2
+    else:
+      i += 1
+  else:
+    raise SyntaxError("File ended while parsing \\cite" + str(args))
+  citations.append(args)
 
 location_arg = None
 
-substitutions = []
+substitutions : list[tuple[Argument, str]] = []
 
 for citation in citations:
-  for arg in citation.args:
-    if isinstance(arg, BracketGroup):
+  for arg in citation:
+    if arg.brackets == '[]':
       if not r'\href' in arg.string:
         location_arg = arg
-    if isinstance(arg, BraceGroup):
+    if arg.brackets == '{}':
       if location_arg and re.match(r'^[PQ][0-9]+$', arg.string):
         artefact = arg.string
         if artefact == 'P222399':
           artefact = 'Q001056'
         location_sources = [s.strip() for s in location_arg.string.split(';')]
-        locations = []
+        locations : list[tuple[str, ...]] = []
         for location in location_sources:
           parts = tuple('o' if part == r'\obverse' else
                         'r' if part == r'\reverse' else
-                        part.split('--')[0] for part in location.split('~'))
+                        part.split('--')[0].replace('′',"'") for part in location.split('~'))
           if not locations or len(parts) >= len(locations[-1]):
             locations.append(parts)
           else:
-            locations.append(locations[-1][:-len(parts)]+parts)
+            locations.append(locations[-1][:-len(parts)] + parts)
         print(arg.string, locations, location_arg)
         print(locations)
         for project in 'etcsri', 'dccmt', 'dcclt', 'epsd2':
@@ -101,10 +134,12 @@ for citation in citations:
           for i, location in enumerate(locations):
             if 'href' in location_sources[i]:
               raise NameError(location_arg, artefact)
-            if location in line_to_id:
-              location_sources[i] = r'\href{http://oracc.org/' + project + '/' + line_to_id[location] + '}{' + location_sources[i] + '}'
-            else:
+            if location not in line_to_id:
               break
+            id = line_to_id[location]
+            if not id:
+              break
+            location_sources[i] = r'\href{http://oracc.org/' + project + '/' + id + '}{' + location_sources[i] + '}'
           else:
             break
         substitutions.append((location_arg, ';'.join(location_sources)))
